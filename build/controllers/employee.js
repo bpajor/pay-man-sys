b60782dcabcd63605a70df73196b3a66f523e404
bpajor <blazej.pajor@wpengine.com>
2024-09-29 13:22:58 +0200

    fix: build not respected by gitignore

--- build/controllers/employee.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getMainPage = void 0;
-const getMainPage = (req, res) => {
-    res.render("common/main");
-};
-exports.getMainPage = getMainPage;
--- build/routes/employee.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.employee_router = void 0;
-const express_1 = require("express");
-const employee_1 = require("../controllers/employee");
-exports.employee_router = (0, express_1.Router)();
-exports.employee_router.get("/", employee_1.getMainPage);
