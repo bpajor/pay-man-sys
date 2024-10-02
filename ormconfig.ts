4555f7ac29592758495837e4e9db9a93cef0709d
bpajor <blazej.pajor@wpengine.com>
2024-10-02 22:01:42 +0200

    feat: add typeORM cli

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,19 +3,22 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const isProduction = process.env.NODE_ENV === "production";
+const is_not_local = process.env.NODE_ENVIRONMENT !== "local";
+console.log(process.env.NODE_ENVIRONMENT);
+console.log(`is_not_local: \n\n\n\n\n\n\n${is_not_local}`);
 
 export const orm_config: DataSourceOptions = {
   type: "postgres",
-  host: isProduction ? undefined : process.env.TYPEORM_HOST,
-  port: isProduction ? undefined : Number(process.env.TYPEORM_PORT),
-  username: isProduction ? undefined : process.env.TYPEORM_USERNAME,
-  password: isProduction ? undefined : process.env.TYPEORM_PASSWORD,
-  database: isProduction ? undefined : process.env.TYPEORM_DATABASE,
-  url: isProduction ? process.env.DATABASE_URL : undefined,
-  entities: ["build/entity/**/*.js", "**/entiy/**/*.ts"],
+  host: is_not_local ? undefined : process.env.TYPEORM_HOST,
+  port: is_not_local ? undefined : Number(process.env.TYPEORM_PORT),
+  username: is_not_local ? undefined : process.env.TYPEORM_USERNAME,
+  password: is_not_local ? undefined : process.env.TYPEORM_PASSWORD,
+  database: is_not_local ? undefined : process.env.TYPEORM_DATABASE,
+  url: is_not_local ? process.env.DATABASE_URL : undefined,
+  entities: ["build/entity/**/*.js"],
+  migrations: ["build/migration/**/*.js"],
   synchronize: process.env.TYPEORM_SYNCHRONIZE === "true",
   logging: process.env.TYPEORM_LOGGING === "true",
-  ssl: isProduction,
-  extra: isProduction ? { ssl: { rejectUnauthorized: false } } : undefined,
+  ssl: is_not_local,
+  extra: is_not_local ? { ssl: { rejectUnauthorized: false } } : undefined,
 };
